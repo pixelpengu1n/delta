3fde2bda9132f27dcd6e2acd22334e69a71c2ea4
pixelpengu1n <aanshu.varmora@outlook.com>
2025-03-19 11:58:26 +1100

    Made minor changes

--- main.py
+++ main.py
@@ -7,19 +7,11 @@ from routes.csv_export import router as csv_export
 from routes.whale_activity import router as whale_activity
 from routes.moving_avg import router as moving_avg
 from routes.sentiments import router as sentiments
-<<<<<<< HEAD
-<<<<<<< HEAD
 from routes.predict import router as predict_router
-from routes.preprocess import router as preprocess
-=======
-=======
->>>>>>> 2e29871 (Made minor changes)
-# from routes.predict import router as predict_router
 from routes.preprocess import router as preprocess
 from routes.analytics import router as analytics
 from routes.visualisation import router as visualise
 
->>>>>>> 2e29871 (Made minor changes)
 app = FastAPI()
 
 app.include_router(greet)
@@ -30,21 +22,10 @@ app.include_router(csv_export)
 app.include_router(whale_activity)
 app.include_router(moving_avg)
 app.include_router(sentiments)
-<<<<<<< HEAD
-<<<<<<< HEAD
 app.include_router(predict_router)
-app.include_router(preprocess)
-=======
-=======
->>>>>>> 2e29871 (Made minor changes)
-# app.include_router(predict_router)
 app.include_router(preprocess)
 app.include_router(analytics)
 app.include_router(visualise)
-<<<<<<< HEAD
->>>>>>> 2e29871 (Made minor changes)
-=======
->>>>>>> 2e29871 (Made minor changes)
 
 if __name__ == "__main__":
     import uvicorn  
