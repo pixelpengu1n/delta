d57cae455883f59129d7192a7c60484f150bc84c
Muhammad Hakiem Bin Zulkifli <z5468915@ad.unsw.edu.au>
2025-03-21 05:58:32 +1100

    Changed port number for deployment

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ app.include_router(yahoo_analyse)
 
 if __name__ == "__main__":
     import uvicorn  
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
